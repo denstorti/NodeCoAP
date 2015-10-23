dcf9707e01416287cad8b77fa9b576303c8a9541
unknown <denstorti@hotmail.com>
2015-10-23 00:20:58 -0200

    alteracao

--- observe_server.js
+++ observe_server.js
@@ -1,6 +1,11 @@
 const coap    = require('coap') // or coap
-    , server  = coap.createServer()
+    //, server  = coap.createServer()
 
+server = coap.createServer(function(req, res) {
+  res.end('Hello ' + req.url.split('/')[1] + '\n')
+  console.log('enviado');
+});
+  
 server.on('request', function(req, res) {
   if (req.headers['Observe'] !== 0)
     return res.end(new Date().toISOString() + '\n')
@@ -17,3 +22,4 @@ server.on('request', function(req, res) {
 server.listen(function() {
   console.log('server started')
 })
+
